2c53b468361d60fabce1c5670c3b0870d99b36fa
zhouleisjj@163.com <zhouleisjj@163.com>
2023-07-07 14:47:44 +0800

    feat:使用URLSearchParams来转换对象，修改环境变量地址内容，修改oauth的使用和导入方法，修改oauth类两个方法请求封装提取

--- src/app/ceshi/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { useSearchParams } from "next/navigation"
-import React from "react"
-
-export default function Page() {
-  const search = useSearchParams().get("oauth_token")
-  console.log(search)
-  return <div>page</div>
-}
