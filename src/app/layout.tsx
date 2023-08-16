16d37fd043a897859ef3bb68795be8b6e4906524
zhoulei <zhouleisjj@163.com>
2023-08-16 11:07:53 +0800

    上传环境变量文件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
+"use client"
 import "./globals.scss"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
-
-export const metadata = {
-  title: "工程化数字管理系统",
-  description: "工程化数字管理系统",
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
